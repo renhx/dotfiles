3148e1108e02bfca47d987683e7c0139a3d2b018
renhx <mx4.gene.g@gmail.com>
2014-09-27 15:15:21 +0900

    Add install_mosh_to_home.sh

---
+++ scripts/install_mosh_to_home.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# ~/usr/local 以下にmoshをインストールする
+
+#make ~/usr/local/
+if [ ! -d $HOME/usr/local/ ]; then
+  mkdir -p $HOME/usr/local/
+fi
+mkdir tmp_install_mosh_to_home && cd tmp_install_mosh_to_home
+
+#install protobuf
+git clone https://github.com/google/protobuf.git
+cd protobuf/
+./autogen.sh && ./configure --prefix=$HOME/usr/local && make && make install
+if [ $? -ne 0 ]; then
+    echo "Error: protobuf"; exit 1
+fi
+cd ..
+
+#install mosh
+#git clone https://github.com/keithw/mosh
+wget http://mosh.mit.edu/mosh-1.2.4.tar.gz
+tar zxvf mosh-1.2.4.tar.gz
+cd mosh-1.2.4
+CXXFLAGS="-I$HOME/usr/local/include" CFLAGS="-I$HOME/usr/local/include" LDFLAGS="-L$HOME/usr/local/lib" PKG_CONFIG_PATH=$HOME/usr/local/lib/pkgconfig ./configure --prefix=$HOME/usr/local/ && make && make install
+if [ $? -ne 0 ]; then
+    echo "Error: mosh"; exit 1
+fi
+cd ..
+
+echo ''
+echo '--------------------------'
+echo 'Append the following lines...'
+echo 'export PATH=$HOME/usr/local/bin/:$PATH'
+echo 'export LD_LIBRARY_PATH=$HOME/usr/local/lib/:$LD_LIBRARY_PATH'
+echo '--------------------------'
