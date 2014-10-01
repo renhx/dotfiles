01c13a3de6399a0509ea3410eed0f08fc0807172
renhx <mx4.gene.g@gmail.com>
2014-10-01 17:15:56 +0900

    Fix: Brewfile -> Brewfile.sh

---
+++ Brewfile.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#Setup
+brew update
+
+#Shell
+brew install bash-completion
+brew install fish --HEAD
+brew install zsh
+brew install zsh-completions
+brew install tmux
+brew install reattach-to-user-namespace
+brew install mobile-shell
+
+#Fonts
+brew tap sanemat/font
+brew install ricty --vim-powerline
+# cp -f /PATH/TO/RICTY/fonts/Ricty*.ttf ~/Library/Fonts/
+# fc-cache -vf
+
+#Langs
+brew install lua
+brew install luajit
+brew install go
+brew install rbenv ruby-build
+brew install pyenv
+brew install nodebrew
+brew install mono
+
+#misc.
+brew install git
+brew install tig
+brew install hub
+brew install tree
+brew install watch
+brew install wget
+brew install htop
+brew install macvim --with-cscope --with-luajit --HEAD # ln -s /usr/local/Cellar/macvim/HEAD/MacVim.app /Applications
+brew install openssl
+brew install ngrok
+
+brew tap peco/peco
+brew install peco
+brew install migemogrep
+
+#Pentest
+brew install sqlmap
+brew install nmap
+brew install hping
+brew install dex2jar
+brew install apktool
+#procyon
+
+#Teardown
+brew cleanup
