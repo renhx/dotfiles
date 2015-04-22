526a18ce41b09b7057d433c1be76277276ce06e3
renhx <me@r3n.cc>
2015-04-22 23:21:08 +0900

    Fix install_tig_to_home.sh

--- scripts/install_tig_to_home.sh
+++ scripts/install_tig_to_home.sh
@@ -7,9 +7,19 @@ if [ ! -d $HOME/usr/local/ ]; then
 fi
 mkdir tmp_install_tig_to_home && cd tmp_install_tig_to_home
 
+#install ncurses
+wget http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz
+tar zxvf ncurses-5.9.tar.gz
+cd ncurses-5.9
+./configure --prefix=$HOME/usr/local && make && make install
+if [ $? -ne 0 ]; then
+    echo "Error: ncurses"; exit 1
+fi
+cd ..
+
 #install tig
 git clone https://github.com/jonas/tig.git && cd tig
-./autogen.sh && ./configure --prefix=$HOME/usr/local && make install
+LIBEVENT_CFLAGS="-I$HOME/usr/local/include" LIBEVENT_LIBS="-L$HOME/usr/local/lib -levent" ./autogen.sh && ./configure --prefix=$HOME/usr/local && make install
 if [ $? -ne 0 ]; then
     echo "Error: tig"; exit 1
 fi
