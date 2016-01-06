eacf47fcc24d4f5b7266add63a8ef1579c835af0
Ren Hirasawa <renhx@users.noreply.github.com>
2016-01-06 16:20:55 +0900

    Update karabiner-us.sh

--- karabiner-us.sh
+++ karabiner-us.sh
@@ -4,16 +4,18 @@ cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 $cli set private.hjkl_to_arrow 1
 /bin/echo -n .
-$cli set remap.jis_command2eisuukana_prefer_command 1
-/bin/echo -n .
 $cli set repeat.initial_wait 416
+/bin/echo -n .
+$cli set private.doublecmd2eisuukana 1
 /bin/echo -n .
 $cli set remap.swapcolons 1
 /bin/echo -n .
-$cli set remap.pc_application2fn 1
-/bin/echo -n .
 $cli set private.wasd_to_arrow 1
 /bin/echo -n .
+$cli set remap.pc_application2fn 1
+/bin/echo -n .
 $cli set private.vim_keybind_apps_esc_with_eisuu 1
+/bin/echo -n .
+$cli set remap.jis_shift2eisuukana_prefer_shift 1
 /bin/echo -n .
 /bin/echo
